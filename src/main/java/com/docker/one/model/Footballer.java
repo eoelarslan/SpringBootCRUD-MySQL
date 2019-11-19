0a6798c6bfd5da7a266a1cbaafff481e2c34be35
eoelarslan <ersin.elarslan@gmail.com>
2019-11-19 23:33:09 +0300

    *Exception Handling *Project Lombok implemented

--- src/main/java/com/docker/one/model/Footballer.java
+++ src/main/java/com/docker/one/model/Footballer.java
@@ -1,6 +1,7 @@
 package com.docker.one.model;
 
 import com.docker.one.model.base.DataEntity;
+import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,6 +11,9 @@ import java.time.LocalDate;
 /**
  * Created by ersin on 16.11.2019.
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "footballer")
 public class Footballer extends DataEntity {
@@ -31,77 +35,4 @@ public class Footballer extends DataEntity {
 
     @Column(name = "worth", nullable = false)
     private int worth;
-
-    public Footballer() {
-        super();
-    }
-
-    public Footballer(String name, String surname, LocalDate dateOfBirth, String dateofPlace, String national, int worth) {
-        this.name = name;
-        this.surname = surname;
-        this.dateOfBirth = dateOfBirth;
-        this.dateofPlace = dateofPlace;
-        this.national = national;
-        this.worth = worth;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(LocalDate dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public String getDateofPlace() {
-        return dateofPlace;
-    }
-
-    public void setDateofPlace(String dateofPlace) {
-        this.dateofPlace = dateofPlace;
-    }
-
-    public String getNational() {
-        return national;
-    }
-
-    public void setNational(String national) {
-        this.national = national;
-    }
-
-    public int getWorth() {
-        return worth;
-    }
-
-    public void setWorth(int worth) {
-        this.worth = worth;
-    }
-
-    @Override
-    public String toString() {
-        return "Footballer{" +
-                "name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", dateOfBirth=" + dateOfBirth +
-                ", dateofPlace='" + dateofPlace + '\'' +
-                ", national='" + national + '\'' +
-                ", worth=" + worth +
-                '}';
-    }
 }
