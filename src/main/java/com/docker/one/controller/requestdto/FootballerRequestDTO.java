f3712958326c4e56689b121fd2b3d59d08fba2b1
eoelarslan <ersin.elarslan@gmail.com>
2019-11-20 21:13:51 +0300

    *Slf4j implemented *Typo fixed *Exception handler updated.

--- src/main/java/com/docker/one/controller/requestdto/FootballerRequestDTO.java
+++ src/main/java/com/docker/one/controller/requestdto/FootballerRequestDTO.java
@@ -1,26 +1,16 @@
 package com.docker.one.controller.requestdto;
 
 import com.docker.one.model.base.DataEntity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 /**
  * Created by ersin on 16.11.2019.
  */
+@ToString
+@Getter
+@Setter
 public class FootballerRequestDTO extends DataEntity {
-
     private int worth;
-
-    public int getWorth() {
-        return worth;
-    }
-
-    public void setWorth(int worth) {
-        this.worth = worth;
-    }
-
-    @Override
-    public String toString() {
-        return "FootballerRequestDTO{" +
-                "worth=" + worth +
-                '}';
-    }
 }
--- src/main/java/com/docker/one/model/Footballer.java
+++ src/main/java/com/docker/one/model/Footballer.java
@@ -6,6 +6,8 @@ import lombok.*;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 /**
@@ -19,20 +21,26 @@ import java.time.LocalDate;
 public class Footballer extends DataEntity {
 
     @Column(name = "name", nullable = false)
+    @NotEmpty(message = "Name cannot be empty")
     private String name;
 
     @Column(name = "surname", nullable = false)
+    @NotEmpty(message = "Surname cannot be empty")
     private String surname;
 
     @Column(name = "date_of_birth", nullable = false)
+    @NotNull(message = "Date of birth cannot be empty")
     private LocalDate dateOfBirth;
 
     @Column(name = "date_of_place", nullable = false)
+    @NotEmpty(message = "Date of place cannot be empty")
     private String dateofPlace;
 
     @Column(name = "national", nullable = false)
+    @NotEmpty(message = "National cannot be empty")
     private String national;
 
     @Column(name = "worth", nullable = false)
+    @NotNull(message = "Worth cannot be empty")
     private int worth;
 }
